fd632dfe75f7f9047c6db3fbf9b0af3b997b9076
Rei0003 <rei03lus@gmail.com>
2026-01-14 12:02:08 +0200

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,15 @@
+
+function clearInput(){
+  var elements = document.getElementsByTagName("input");
+  for (var i=0; i < elements.length; i++) {
+    if (elements[i].type == "text") {
+      elements[i].value = "";
+    }
+  }
+  var selects = document.getElementsByTagName("select");
+  for (var i=0; i < elements.length; i++) {
+    if (selects[i].type == "text") {
+      selects[i].value = "";
+    }
+  }
+}
